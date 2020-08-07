206e403c6123e6b6b016156655b717908faadf21
Joe Muoio <joegm258@gmail.com>
2020-08-07 14:57:55 -0700

    Revert "Adding ja-JP version"
    This reverts commit b9667ef40331b77377d067b5568714354e51d6de.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -10,7 +10,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'ようこそ。こんにちは、または、ヘルプ、と言ってみてください。どうぞ！';
+        const speakOutput = 'नमस्ते, आप hello या help कह सकते हैं. आप क्या करना चाहेंगे?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -25,7 +25,7 @@ const HelloWorldIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'ハローワールド';
+        const speakOutput = 'नमस्ते दुनिया ';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -40,7 +40,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'こんにちは、と言ってみてください。どうぞ！';
+        const speakOutput = 'आप मुझसे hello बोल सकते हो.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -56,7 +56,7 @@ const CancelAndStopIntentHandler = {
                 || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'さようなら';
+        const speakOutput = 'अलविदा ';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -74,7 +74,7 @@ const FallbackIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'ごめんなさい。ちょっとよくわかりませんでした。もう一度言ってみてください。';
+        const speakOutput = 'Sorry, मैं वो समझ नहीं पायी. क्या आप दोहरा सकते हैं ';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -108,7 +108,7 @@ const IntentReflectorHandler = {
     },
     handle(handlerInput) {
         const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `${intentName}がトリガーされました。`;
+        const speakOutput = `आपने ${intentName} trigger किया हैं `;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -126,7 +126,7 @@ const ErrorHandler = {
         return true;
     },
     handle(handlerInput, error) {
-        const speakOutput = 'ごめんなさい。なんだかうまく行かないようです。もう一度言ってみてください。';
+        const speakOutput = 'Sorry, मैं वो समझ नहीं पायी. क्या आप दोहरा सकते हैं ';
         console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
 
         return handlerInput.responseBuilder
